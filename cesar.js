e6355c5395738ec3df18a7c64d94c901ff1d44ba
Cleide sales <salessilva@acad.ifma.edu.br>
2024-11-19 20:09:37 -0300

    Add files via upload

--- cesar.js
+++ cesar.js
@@ -40,13 +40,3 @@ function decifraLetra(letra, chave, alfabeto) {
     return resultado;
 }
 
-const mensagem = "Javascript Cifra de Cesar";
-const chave = 5;
-
-console.log("Mensagem original: ", mensagem);
-
-const mensagemCifrada = criptografia(mensagem, chave, cifraLetra);
-console.log("Mensagem cifrada: ", mensagemCifrada);
-
-const mensagemDecifrada = criptografia(mensagemCifrada, chave, decifraLetra);
-console.log("Mensagem decifrada: ", mensagemDecifrada);
