5f05a2986bed05ff13ad25c06b0f2d5ff8f48135
Cleide <salessilva@acad.ifma.edu.br>
2024-11-19 20:39:16 -0300

    t

--- cesar.js
+++ cesar.js
@@ -39,4 +39,14 @@ function decifraLetra(letra, chave, alfabeto) {
     }
     return resultado;
 }
+const mensagem = "Javascript Cifra de Cesar";
+const chave = 5;
+
+console.log("Mensagem original: ", mensagem);
+
+const mensagemCifrada = criptografia(mensagem, chave, cifraLetra);
+console.log("Mensagem cifrada: ", mensagemCifrada);
+
+const mensagemDecifrada = criptografia(mensagemCifrada, chave, decifraLetra);
+console.log("Mensagem decifrada: ", mensagemDecifrada);
 
